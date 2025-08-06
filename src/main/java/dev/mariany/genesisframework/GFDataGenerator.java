c1b82ddda4e2a3ba885dab5305b625e3c4c30fb8
Mariany Pinkston <marianypinkston@gmail.com>
2025-08-05 23:23:42 -0400

    Implement getEffectiveModId to data generator

--- src/main/java/dev/mariany/genesisframework/GFDataGenerator.java
+++ src/main/java/dev/mariany/genesisframework/GFDataGenerator.java
@@ -4,12 +4,18 @@ import dev.mariany.genesisframework.datagen.GFInstructionsProvider;
 import dev.mariany.genesisframework.datagen.GFModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.jetbrains.annotations.Nullable;
 
 public class GFDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-		pack.addProvider(GFModelProvider::new);
-		pack.addProvider(GFInstructionsProvider::new);
-	}
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(GFModelProvider::new);
+        pack.addProvider(GFInstructionsProvider::new);
+    }
+
+    @Override
+    public @Nullable String getEffectiveModId() {
+        return GenesisFramework.MOD_ID;
+    }
 }
